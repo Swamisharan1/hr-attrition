7f1c2c6d665596ec3fd8482a6e2fe4bf375b7537
Swamisharan1 <147104986+Swamisharan1@users.noreply.github.com>
2023-10-06 03:06:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,57 +3,65 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+import requests  # Added for fetching data from GitHub
 
 st.title("Employee Attrition Prediction")
 
-# Upload a CSV file with data
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+# GitHub raw CSV URL
+github_raw_csv_url = 'https://raw.githubusercontent.com/Swamisharan1/hr-attrition/main/HR-Employee-Attrition.csv'
 
-if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-    df = pd.DataFrame(data)
+# Function to fetch data from GitHub
+def fetch_data_from_github(url):
+    response = requests.get(url)
+    return response.text
 
-    # Data preprocessing
-    from sklearn.preprocessing import LabelEncoder
-    labelencoder = LabelEncoder()
+# Fetch the CSV data from GitHub
+csv_data = fetch_data_from_github(github_raw_csv_url)
 
-    for column in df.columns:
-        if df[column].dtype == object:
-            df[column] = labelencoder.fit_transform(df[column])
+# Load data into a DataFrame
+df = pd.read_csv(pd.compat.StringIO(csv_data))
 
-    X = df.drop('Attrition', axis=1)
-    y = df['Attrition']
+# Data preprocessing
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
 
-    # Split data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+for column in df.columns:
+    if df[column].dtype == object:
+        df[column] = labelencoder.fit_transform(df[column])
 
-    # Train a logistic regression model
-    logreg = LogisticRegression()
-    logreg.fit(X_train, y_train)
+X = df.drop('Attrition', axis=1)
+y = df['Attrition']
 
-    # User input for prediction
-    st.header("Predict Employee Attrition")
-    st.write("Enter employee data for prediction:")
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    # Create input fields for the user to enter data
-    input_data = {}
-    for col in X.columns:
-        input_data[col] = st.number_input(col)
+# Train a logistic regression model
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
 
-    if st.button("Predict"):
-        # Convert input data to a DataFrame
-        input_df = pd.DataFrame([input_data])
+# User input for prediction
+st.header("Predict Employee Attrition")
+st.write("Enter employee data for prediction:")
 
-        # Preprocess input data
-        for column in input_df.columns:
-            if input_df[column].dtype == object:
-                input_df[column] = labelencoder.transform(input_df[column])
+# Create input fields for the user to enter data
+input_data = {}
+for col in X.columns:
+    input_data[col] = st.number_input(col)
 
-        # Make predictions
-        prediction = logreg.predict(input_df)
+if st.button("Predict"):
+    # Convert input data to a DataFrame
+    input_df = pd.DataFrame([input_data])
 
-        # Display the prediction result
-        if prediction[0] == 1:
-            st.warning("Employee is likely to leave (Attrition: Yes).")
-        else:
-            st.success("Employee is likely to stay (Attrition: No).")
+    # Preprocess input data
+    for column in input_df.columns:
+        if input_df[column].dtype == object:
+            input_df[column] = labelencoder.transform(input_df[column])
+
+    # Make predictions
+    prediction = logreg.predict(input_df)
+
+    # Display the prediction result
+    if prediction[0] == 1:
+        st.warning("Employee is likely to leave (Attrition: Yes).")
+    else:
+        st.success("Employee is likely to stay (Attrition: No).")
