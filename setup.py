023cd5b92b70702d50cf0422a36d6a87349926ea
Swamisharan1 <147104986+Swamisharan1@users.noreply.github.com>
2023-10-06 02:42:16 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yourpackage',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit==0.89.0',
+        'pandas==1.3.3',
+        'scikit-learn==0.24.2',
+    ],
+)
